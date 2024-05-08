da334e484d1d0b919baf34b821701c8ffb37027f
kateryna <k.skoryna@gmail.com>
2024-05-08 12:17:51 +0200

    add activity component

--- src/ActivityComponent/ActivityComponent.tsx
+++ src/ActivityComponent/ActivityComponent.tsx
@@ -1,9 +1,10 @@
 import { Activity } from "../types/types";
+import { Card } from "antd";
 
 export const ActivityComponent: React.FC<Activity> = ({ activity }) => {
   return (
-    <div>
-      <p>Activity: {activity}</p>
-    </div>
+    <Card bordered={false} style={{ width: 300 }}>
+      <p>{activity}</p>
+    </Card>
   );
 };
