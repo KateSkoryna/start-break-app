da2ac7d18a88c580c8914018900d6cc5ae43e525
kateryna <k.skoryna@gmail.com>
2024-05-08 11:52:23 +0200

    add comment

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,8 @@ const router = createBrowserRouter(
   }
 );
 
+//add basename
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
